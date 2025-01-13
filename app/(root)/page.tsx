d785cf62424228039ac239bd6dd642d6b945c0f2
Jamison Orton <jorton4@byu.edu>
2025-01-13 16:33:28 -0700

    added sampledata to test card components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
