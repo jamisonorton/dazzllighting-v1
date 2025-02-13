a3f843148c30df97983398f29b9ac6b91879ce2f
Jamison Orton <jorton4@byu.edu>
2025-02-13 15:18:28 -0700

    test

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -9,8 +9,6 @@ import {
 import { ChevronLeft, ShoppingCart, UserIcon } from "lucide-react";
 import Link from "next/link";
 import UserButton from "./user-button";
-import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
 
 const links = [
   {
@@ -24,26 +22,25 @@ const links = [
 ];
 
 const Menu = () => {
-  const pathname = usePathname();
   return (
     <div className="flex justify-end gap-3">
       <nav className="hidden md:flex w-full-max-w-xs gap-1">
-        {links.map((item) => (
-          <Button asChild variant="ghost" key={item.href}>
-            <Link
-              href={item.href}
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-primary",
-                pathname.includes(item.href) ? "" : "text-muted-foreground"
-              )}
-            >
-              {item.title}
-            </Link>
-          </Button>
-        ))}
+        <Button asChild variant="ghost">
+          <Link href="/permanent">Lighting</Link>
+        </Button>
+        <Button asChild variant="ghost">
+          <Link href="/bulbs">Bulbs</Link>
+        </Button>
+        <Button asChild variant="ghost">
+          <Link href="/stringlights">String Lights</Link>
+        </Button>
+        <Button asChild variant="ghost">
+          <Link href="/patio">Patio Lights</Link>
+        </Button>
         <Button asChild variant="ghost">
           <Link href="/cart">
-            <ShoppingCart /> Cart
+            <ShoppingCart />
+            Cart
           </Link>
         </Button>
         <UserButton />
