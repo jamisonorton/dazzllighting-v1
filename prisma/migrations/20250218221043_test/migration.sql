c20b0db61d29a5a1ec1e3cd0b212e141a23a9651
Jamison Orton <jorton4@byu.edu>
2025-02-19 16:35:50 -0700

    removed old migrations

--- prisma/migrations/20250218221043_test/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "OrderItem" RENAME CONSTRAINT "orderitems_orderId_productId_pk" TO "orderItems_orderId_productId_pk";
--- prisma/migrations/20250218221413_updated_order_items/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "OrderItem" RENAME CONSTRAINT "orderItems_orderId_productId_pk" TO "orderitems_orderId_productId_pk";
